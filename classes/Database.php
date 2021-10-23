b88e262953c5808580d9fa8ca231127c3548833d
Mayowa Ajagunna <sagaraaj69@gmail.com>
2021-10-23 21:17:43 +0100

    Update Database.php

--- classes/Database.php
+++ classes/Database.php
@@ -1,16 +1,19 @@
 <?php
 class Database{
     
-    // CHANGE THE DB INFO ACCORDING TO YOUR DATABASE
-    private $db_host = 'localhost';
-    private $db_name = 'trove';
-    private $db_username = 'root';
-    private $db_password = '';
+    //Get Heroku ClearDB connection information
+private $cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+private $cleardb_server = $cleardb_url["host"];
+private $cleardb_username = $cleardb_url["user"];
+private $cleardb_password = $cleardb_url["pass"];
+private $cleardb_db = substr($cleardb_url["path"],1);
+private $active_group = 'default';
+private $query_builder = TRUE;
     
     public function dbConnection(){
         
         try{
-            $conn = new PDO('mysql:host='.$this->db_host.';dbname='.$this->db_name,$this->db_username,$this->db_password);
+            $conn = new PDO('mysql:host='.$this->cleardb_server.';dbname='.$this->cleardb_db,$this->cleardb_username,$this->cleardb_password);
             $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             return $conn;
         }
